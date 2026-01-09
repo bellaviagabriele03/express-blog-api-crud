a30e8c467c2520f49cd729cf8c8f9bfa0eda5f0d
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-09 04:59:29 +0100

    fix: messaggio di errore per il notfound

--- middlewares/notfound.js
+++ middlewares/notfound.js
@@ -1,6 +1,6 @@
 export default function notfound(req, res, next) {
     res.status(404).json({
         errore: "404",
-        message: "errore dati non trovati chiedi a loris !!"
+        message: "errore rotta non trovata chiedi a loris !!"
     })
 }   
